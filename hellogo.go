3a984ba8b74bb0985c8a45926beb04cea3c7b2f7
Powwow84 <pao.saelee84@gmail.com>
2023-07-20 09:54:48 -0700

    refreshed more on bsics of operators and time

--- hellogo.go
+++ hellogo.go
@@ -3,10 +3,15 @@ package main
 import (
 	// "bufio"
 	"fmt"
-	"strconv"
+	// "strconv"
+	// "strings"
+	// "unicode/utf8"
+
 	// "log"
 	// "os"
-	"reflect"
+	// "reflect"
+	// "time"
+	"math"
 )
 
 var pl = fmt.Println
@@ -82,4 +87,112 @@ func main() {
 	// }
 
 	// pl("!true =", !true)
+
+
+	// ---------------------------strings
+
+	// sv1 := "A word"
+	// replacer := strings.NewReplacer("A", "Another" )
+	// sV2 := replacer.Replace(sV1)
+	// pl(sv2)
+
+	// pl("Length :", len(sV2))
+	// pl("Contains Another :", strings.Contains(sV2,"Another"))
+	// pl("o index:", strings.Index(sV2, "o"))
+	// pl("Replace :", strings.Replace(sV2, "o", "O", -1)) // -1 replaces everything, if you had a 2 it would replace the first two
+	// sV3 := "\nSome Words\n" // the  \ lead into an escape sequence, lets you use things insdie of quotes like """ " 
+	// sV3 = strings.TrimSpace(sV3) //trims whitespace
+	// pl("Split :", strings.Split("a-b-c-d", "-"))
+	// pl("lower :", strings.ToLower(sV2))
+	// pl("upper :", strings.ToUpper(sV2))
+	// pl("Prefix :", strings.HasPrefix("tacocat", "taco"))
+	// pl("Suffix :", strings.HasSuffix("tacocat", "cat"))
+	
+	// ---------------runes
+	// characters are called runes, runes are unicode that represents characters
+
+	// rStr := "abcdefg"
+	// pl("Rune COunt :", utf8.RuneCountInString(rStr))
+	// for i, runeVal := range rStr{
+	// 	fmt.Printf("%d : %#U : %c\n", i, runeVal, runeVal ) //<- formated printing. Note whenever you use Printf you need to use \n to break it into a new line if you want a new line
+	// 	// returns this
+	// 	// Rune COunt : 7
+	// 	// 0 : U+0061 'a' : a
+	// 	// 1 : U+0062 'b' : b
+	// 	// 2 : U+0063 'c' : c
+	// 	// 3 : U+0064 'd' : d
+	// 	// 4 : U+0065 'e' : e
+	// 	// 5 : U+0066 'f' : f
+	// 	// 6 : U+0067 'g' : g
+	// }
+
+
+	// -------------------Time
+	// now := time.Now()
+	// pl(now.Year(), now.Month(), now.Day())
+	// pl(now.Hour(), now.Minute(), now.Second())
+
+	// // -----------------operators
+
+	// pl("5+4 =" , 5+4)
+	// pl("5-4 =" , 5-4)
+	// pl("5*4 =" , 5*4)
+	// pl("5/4 =" , 5/4)
+	// pl("5%4 =" , 5%4)
+	// mInt := 1
+	// mInt++
+	// mInt += 1
+	// mInt--
+	// mInt -=
+
+	// -----------------Mathmathical functions
+		// Most commonly used
+
+	pl("Abs(-10) =", math.Abs(-10))
+	pl("Pow(4, 2) =", math.Pow(4, 2))
+	pl("Cbrt(8) =", math.Cbrt(8))
+	pl("Ceil(4.4) =", math.Ceil(4.4))
+	pl("floor(4.4) =", math.Floor(4.4))
+	pl("Round(4.4) =", math.Round(4.4))
+	pl("Log2(8) =", math.Log2(8))
+	pl("Log10(100) =", math.Log10(100))
+	// get the log of e to the power of 2
+	pl("Log(7.389) =", math.Log(math.Exp(2)))
+	pl("Max(5,4) =", math.Max(5,4))
+	pl("Min(5,4) =", math.Min(5,4))
+	// converts 90 degrees into radians
+	r90 := 90 * math.Pi / 180
+	// converts radians into degrees
+	d90 := r90 * (180 / math.Pi)
+	fmt.Printf("%f radians = %f degrees\n", r90, d90)
+	pl("Sin(90) =", math.Sin(r90))
+	// there are also functions dor cos, tan, acos, asin
+	// Atan, AsinH, Acosh, Atanh, Atan2, Cosh, Sinh, Sincos, Htpos
+
+	// Abs(-10) = 10 (absolute value)
+	// Pow(4, 2) = 16 (power of)
+	// Cbrt(8) = 2 (cube)
+	// Ceil(4.4) = 5 (ceiling)
+	// floor(4.4) = 4 (floor)
+	// Round(4.4) = 4 (round up or down)
+	// Log2(8) = 3 he math.Log2 function takes a single argument of type float64 and returns the base-2 logarithm as a float64 value. In this case, the result would be 3 because 2 raised to the power of 3 equals 8.
+	// Log10(100) = 2 unction takes a single argument of type float64 and returns the base-10 logarithm as a float64 value. In this case, the result would be 2 because 10 raised to the power of 2 equals 100.
+	// Log(7.389) = 2 The math.Log function takes a single argument of type float64 and returns the natural logarithm as a float64 value. In this case, the result would be approximately 2 because e raised to the power of 2 equals 7.389, where e is Euler's number approximately equal to 2.71828.
+	// Max(5,4) = 5
+	// Min(5,4) = 4
+
+	
+
+	// // float precision
+	// pl("Float Precision = ", 0.11111111111 + 0.11111111111) // important to understand percision when dealing with floats. when doing the calculation it will hold all the decimal places
+
+	// ---------------random numbers
+
+	// seedSecs := time.Now().Unix() //returns seconds since 1/1/70 as an int
+	// random.Seed(seedSecs)
+	// randNum := rand.Intn(50) + 1 // generates a random number exclusive of 50 (0 -49). So we add the +1 to generate it to 0 - 50
+	// pl("Random : ", randNum)
+
+
+	
 }
